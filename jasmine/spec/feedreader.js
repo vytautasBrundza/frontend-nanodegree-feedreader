8ebf9f18e76ebd8b5c329be29e80e209d80edd4f
Vytautas Brundza <vytautas.brundza@cohaesus.co.uk>
2015-07-27 21:11:20 +0100

    All tests working

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -35,7 +35,7 @@ $(function() {
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url.length).not.toBe(0);
-            };
+            }
         });
 
         /* A test that loops through each feed
@@ -47,7 +47,7 @@ $(function() {
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
                 expect(allFeeds[i].name.length).not.toBe(0);
-            };
+            }
         });
     });
 
@@ -80,30 +80,69 @@ $(function() {
         });
 
         it('hides when clicked again', function() {
-            // simulate the click
+            // simulate two clicks
+            button.click();
+            // check if class is removed
+            expect(document.body.className).toEqual("");
             button.click();
             // check if class is applied again
             expect(document.body.className).toEqual("menu-hidden");
         });
     });
 
-    /* A new test suite named "Initial Entries" */
-    describe('Initial Entries', function() {
-        /* A test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+    /* A new test suite named 'Initial Entries' */
+    /* A test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
+    describe('Initial Entries', function () {
+
+        beforeEach(function (done) {
+            // Make an async call, passing the special done callback
+            loadFeed(0,done);
+        });
+
+        it('there is at least one entry', function() {
+            // print number of entries
+            console.log("there are "+$('.entry').length+" entries. 1 required.");
+            expect($('.entry').length).not.toBe(0);
+        });
+
+        it('there are maximum number (four) of entries', function() {
+            // print number of entries
+            console.log("there are "+$('.entry').length+" entries. 4 required.");
+            expect($('.entry').length).toBe(4);
+        });
     });
 
+    // (https://github.com/jasmine/jasmine/issues/526)
+    /* A test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
+    describe('New Feed Selection', function () {
+        var middleState;
+        var finalState;
 
-    /* A new test suite named "New Feed Selection" */
-    describe('New Feed Selection', function() {
-        /* A test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+        it("the contents have changed", function(done) {
+            // load first feed
+            loadFeed(0,function() {
+                // get anchor href of the first feed message
+                console.log($($(".feed a")[0]).attr('href'));
+                middleState=$($(".feed a")[0]).attr('href');
+                // laod second feed
+                loadFeed(1,function() {
+                    // get anchor href of the first feed message
+                    console.log($($(".feed a")[0]).attr('href'));
+                    finalState=$($(".feed a")[0]).attr('href');
+                    // compare first feed element anchor href values
+                    expect(finalState).not.toEqual(middleState);
+                    done();
+                });
+            });
+        });
     });
-
 }());
+
