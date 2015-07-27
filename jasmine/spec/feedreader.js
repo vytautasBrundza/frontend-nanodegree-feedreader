367847dd6a7e3f8e3f5e86353d2e89b569bd8fbd
Vytautas Brundza <vytautas.brundza@cohaesus.co.uk>
2015-07-27 14:59:27 +0100

    RSS Feeds working

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -42,6 +42,32 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
 
+        describe('The menu', function() {
+        /* A test that loops through each feed
+         * in the allFeeds object and ensures it has a URL defined
+         * and that the URL is not empty.
+         */
+        it('links are defined', function() {
+            var allURLS=[];
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+            };
+        });
+
+        /* A test that loops through each feed
+         * in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty.
+         */
+         it('name is defined', function() {
+            var allURLS=[];
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            };
+        });
+    });
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
